1734d21e0c77181cd6e21438680e5f5769606c2f
Jonah Lewis <lewisj41@unlv.nevada.edu>
2025-02-12 21:47:48 -0800

    asdd validation directory

---
+++ step01-Model.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+TRAINING_DIRECTORY = '../data/training'
+TESTING_DIRECTORY = '../data/test'
+VALIDATION_DIRECTORY = '../data/validation'
